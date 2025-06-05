365fd65a30573860e6852e5df71af8841f98b387
Ana <ana_l_amaral@estudante.sesisenai.org.br>
2025-06-05 08:16:17 -0300

    Add - botao

--- scripts/script.js
+++ scripts/script.js
@@ -4,3 +4,6 @@ function myTimer() {
   const d = new Date();
   document.getElementById("demo").innerHTML = d.toLocaleTimeString();
 }
+
+const collapseElementList = document.querySelectorAll('.collapse')
+const collapseList = [...collapseElementList].map(collapseEl => new bootstrap.Collapse(collapseEl))
