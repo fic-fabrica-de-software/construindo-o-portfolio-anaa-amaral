82d574256104e5476e32e0001c4661ebd52badbb
Ana <ana_l_amaral@estudante.sesisenai.org.br>
2025-06-12 16:08:37 -0300

    Add

--- scripts/script.js
+++ scripts/script.js
@@ -4,3 +4,6 @@ function myTimer() {
   const d = new Date();
   document.getElementById("data").innerHTML = d.toLocaleTimeString();
 }
+
+const exampleEl = document.getElementById('example')
+const popover = new bootstrap.Popover(exampleEl, options)
