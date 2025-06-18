a3c2b23d73a1fdf9385194813f34e9e8a9b2cde7
Ana <ana_l_amaral@estudante.sesisenai.org.br>
2025-06-18 07:25:04 -0300

    Add - link

--- scripts/script.js
+++ scripts/script.js
@@ -7,3 +7,24 @@ function myTimer() {
 
 const exampleEl = document.getElementById('example')
 const popover = new bootstrap.Popover(exampleEl, options)
+
+function createHeart() {
+  const heart = document.createElement('div');
+  heart.classList.add('heart');
+
+  heart.style.left = Math.random() * window.innerWidth + '3px';
+
+  const size = Math.random() * 20 + 10; 
+  heart.style.width = size + '3px';
+  heart.style.height = size + '3px';
+
+  heart.style.animationDuration = (Math.random() * 2 + 3) + '2s';
+
+  document.querySelector('.container').appendChild(heart);
+
+  setTimeout(() => {
+    heart.remove();
+  }, 5000);
+}
+
+setInterval(createHeart, 200);
